ff823cf51d49785425d42e60e0bf13e27b1cc5dd
Maryshajina <66456057+Maryshajina@users.noreply.github.com>
2020-06-09 18:05:46 +0530

    Create Assignment 3.py

---
+++ Assignment 3.py
@@ -0,0 +1,17 @@
+1) names: ["Mike", "Joy", "Kate"] 
+   names.append(Leah) 
+      print(names) 
+
+2) tup1: ("Physics", "Chemistry", "Biology", "Math") 
+   tup2: ("Scientist", "Chemist", Biologist", "Mathematician ") 
+       print "tup1[0]"
+
+3) myDict = {
+     "Rose" = "Red", 
+     "Lily" = "White", 
+     "Lotus"= "Pink", 
+     "Sunflower" = "Yellow"
+  } 
+     delmyDict("Lotus") 
+     printmyDict 
+ 

37514785df173c374088fd0f23fde24b987ef553
Maryshajina <66456057+Maryshajina@users.noreply.github.com>
2020-06-10 14:14:17 +0530

    Made changes

--- Assignment 3.py
+++ Assignment 3.py
@@ -1,17 +1,17 @@
-1) names: ["Mike", "Joy", "Kate"] 
-   names.append(Leah) 
+names: ["Mike", "Joy", "Kate"] 
+names.append(Leah) 
       print(names) 
 
-2) tup1: ("Physics", "Chemistry", "Biology", "Math") 
-   tup2: ("Scientist", "Chemist", Biologist", "Mathematician ") 
+tup1: ("Physics", "Chemistry", "Biology", "Math") 
+tup2: ("Scientist", "Chemist", Biologist", "Mathematician ") 
        print "tup1[0]"
 
-3) myDict = {
-     "Rose" = "Red", 
-     "Lily" = "White", 
-     "Lotus"= "Pink", 
-     "Sunflower" = "Yellow"
+myDict = {
+  "Rose" = "Red", 
+  "Lily" = "White", 
+  "Lotus"= "Pink", 
+  "Sunflower" = "Yellow"
   } 
-     delmyDict("Lotus") 
-     printmyDict 
+ delmyDict("Lotus") 
+  printmyDict 
  
